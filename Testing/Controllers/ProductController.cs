a9c57a534d016428a5026c9fb3175387b7e93fbc
heather otto <heatherotto32@yahoo.com>
2022-11-23 13:32:29 -0600

    update

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
 
 namespace Testing.Controllers
 {
@@ -18,6 +19,22 @@ namespace Testing.Controllers
             return View(products);
         }
 
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
         public IActionResult ViewProduct(int id)
         {
             var product = repo.GetProduct(id);
